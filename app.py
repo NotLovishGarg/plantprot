fed432ed1bc83ecf40d0a13d40a49c490547fa5d
Lovish <lovishgarg33@gmail.com>
2024-11-04 12:15:02 +0530

    prediction still wrong

--- app.py
+++ app.py
@@ -7,21 +7,30 @@ import io
 app = Flask(__name__)
 
 # Load the model (adjust path as needed)
-model = tf.keras.models.load_model(r'C:\Users\lovis\Desktop\PlantProtect\trained_model.keras')
+model = tf.keras.models.load_model('./plant_trained.keras')
 
 # Define your class labels
-class_labels = [
-    'tomato-disease1',
-    'tomato-disease2',
-    # Add all your disease classes here
-]
+class_labels = ['Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy',
+                    'Blueberry___healthy', 'Cherry_(including_sour)___Powdery_mildew', 
+                    'Cherry_(including_sour)___healthy', 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot', 
+                    'Corn_(maize)___Common_rust_', 'Corn_(maize)___Northern_Leaf_Blight', 'Corn_(maize)___healthy', 
+                    'Grape___Black_rot', 'Grape___Esca_(Black_Measles)', 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 
+                    'Grape___healthy', 'Orange___Haunglongbing_(Citrus_greening)', 'Peach___Bacterial_spot',
+                    'Peach___healthy', 'Pepper,_bell___Bacterial_spot', 'Pepper,_bell___healthy', 
+                    'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy', 
+                    'Raspberry___healthy', 'Soybean___healthy', 'Squash___Powdery_mildew', 
+                    'Strawberry___Leaf_scorch', 'Strawberry___healthy', 'Tomato___Bacterial_spot', 
+                    'Tomato___Early_blight', 'Tomato___Late_blight', 'Tomato___Leaf_Mold', 
+                    'Tomato___Septoria_leaf_spot', 'Tomato___Spider_mites Two-spotted_spider_mite', 
+                    'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus',
+                      'Tomato___healthy']
 
 def preprocess_image(image_bytes):
     # Convert bytes to image
     image = Image.open(io.BytesIO(image_bytes))
     
     # Resize image to match model input size (adjust size as needed)
-    image = image.resize((224, 224))
+    image = image.resize((128, 128))
     
     # Convert to array and preprocess
     img_array = tf.keras.preprocessing.image.img_to_array(image)
@@ -35,6 +44,14 @@ def preprocess_image(image_bytes):
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('ll.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('sign.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
